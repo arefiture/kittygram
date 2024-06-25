1ab0d77b6144ea1248c05a0d17f76485bf3e59d7
Aleksandr Rakov <alexrinko@hotmail.com>
2024-06-26 04:17:54 +0500

    Использование Generic'ов

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from cats.views import APICat, cat_detail
+from cats.views import CatDetail, CatList
 
 urlpatterns = [
-   path('cats/', APICat.as_view()),
-   path('cats/<int:pk>/', cat_detail),
+   path('cats/', CatList.as_view()),
+   path('cats/<int:pk>/', CatDetail.as_view()),
 ]
